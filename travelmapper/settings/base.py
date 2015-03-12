5260c6fd851c85934cb967b81cc65f26f2f2fa74
Jeremy Lach <lach.jeremy@gmail.com>
2015-03-11 18:18:15 -1000

    settings

--- travelmapper/settings/base.py
+++ travelmapper/settings/base.py
@@ -138,6 +138,7 @@ STATICFILES_DIRS = (
     #os.path.join(BASE_DIR, "static"),
     #PROJECT_DIR.child("static"),
     os.path.join(PROJECT_DIR, "map/static/map"),
+    os.path.join(BASE_DIR, 'static')
 )
 STATIC_ROOT = PROJECT_DIR.child("static")
 #STATIC_ROOT = '/Developer/django/travelmapper/staticfiles/'
