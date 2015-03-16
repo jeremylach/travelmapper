669f657f5d145ccc18c2e7b46c9064c0c88a23a4
Jeremy Lach <lach.jeremy@gmail.com>
2015-03-15 18:11:03 -1000

    admin url

--- travelmapper/urls.py
+++ travelmapper/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'travelmapper.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', include(admin.site.urls)),    
+    url(r'^/admin/admin/', include(admin.site.urls)),    
     url(r'^', include('map.urls')),
     
     url(r'', include('social_auth.urls')),
