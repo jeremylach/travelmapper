d1bcec9cdcb964a601cacdaf71db8cf7883752e8
Jeremy Lach <lach.jeremy@gmail.com>
2015-03-15 16:02:48 -1000

    production settings

--- travelmapper/settings/production.py
+++ travelmapper/settings/production.py
@@ -18,7 +18,7 @@ DEBUG = True
 #    PROJECT_DIR.child("static"), 
 #)
 
-DATABASES = {}
+#DATABASES = {}
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
@@ -30,14 +30,14 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 #STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 
 
