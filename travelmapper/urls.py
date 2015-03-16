93940b00b3460ef7f222b6c73f0cc1f2ac883f1e
Jeremy Lach <lach.jeremy@gmail.com>
2015-03-15 18:03:34 -1000

    admin url

--- travelmapper/urls.py
+++ travelmapper/urls.py
@@ -6,13 +6,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'travelmapper.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include('map.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
 
 )
 
-urlpatterns += map.urls.urlpatterns
+#urlpatterns += map.urls.urlpatterns
 
 #from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #urlpatterns += staticfiles_urlpatterns()
