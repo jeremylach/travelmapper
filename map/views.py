8b44d9f17f027c92c85cd5dcfe9a420b23e91900
Jeremy Lach <lach.jeremy@gmail.com>
2015-03-17 14:44:27 -1000

    realtime api

--- map/views.py
+++ map/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import *
+from social_auth.db.django_models import *
 from instagram import client, subscriptions
 from django.contrib.auth import logout as auth_logout
 from instagram import client, subscriptions
