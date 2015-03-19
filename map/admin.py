6b8bcc30cf4c14c72c0f4b8355ed89a0779ec23d
Jeremy Lach <lach.jeremy@gmail.com>
2015-03-18 19:46:59 -1000

    admin

--- map/admin.py
+++ map/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import *
 
 class PhotoMomentAdmin(admin.ModelAdmin):
-    list_filter = ("user",) 
+    list_filter = ("user",)
+    list_display = ['user', 'created', 'media_type']
 
 admin.site.register(PhotoMoment, PhotoMomentAdmin)
 
