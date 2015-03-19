02b0a48117cbdc4e2fafea2a5e68b4528d690184
Jeremy Lach <lach.jeremy@gmail.com>
2015-03-18 19:59:54 -1000

    admin

--- map/admin.py
+++ map/admin.py
@@ -3,7 +3,7 @@ from .models import *
 
 class PhotoMomentAdmin(admin.ModelAdmin):
     list_filter = ("user",)
-    list_display = ['user', 'created', 'get_media_type_display']
+    list_display = ['name', 'user', 'created', 'get_media_type_display']
 
 admin.site.register(PhotoMoment, PhotoMomentAdmin)
 
